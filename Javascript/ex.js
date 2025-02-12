d24adcab3f9855c482455d88ed4adc6d09470d0c
khushbubhatt2306 <khushbu.bhatt2306@gmail.com>
2025-02-12 17:21:22 +0530

    js

---
+++ Javascript/ex.js
@@ -0,0 +1,10 @@
+console.log("External file");
+
+document.write("hello document")
+
+// bom :- borwser object model 
+
+// alert("pls field required")
+// confirm("Are you sure?")
+// prompt("5 + 5 ? => ")
+// print()
